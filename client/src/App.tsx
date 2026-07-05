b07235210738765dda12a7b53d8c083b64b9cca6
Muhamed bassal <69437958+Bassal-11@users.noreply.github.com>
2026-07-05 22:14:42 +0300

    Implement Products page with filtering, sorting, and breadcrumb navigation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,23 +15,23 @@ import ProtectedRoute from './components/ProtectedRoute';
 const App = () => {
     return (
         <>
-            <Toaster position='top-right' toastOptions={{ duration: 3000, style: { fontSize: '14px', backgroundColor: '#183022', color: '#fff', borderRadius: '12px' } }} />
+            <Toaster position="top-right" toastOptions={{ duration: 3000, style: { fontSize: '14px', backgroundColor: '#183022', color: '#fff', borderRadius: '12px' } }} />
             <Routes>
                 {/* Authenticated routes  */}
-                <Route path='/login' element={<Login />} />
+                <Route path="/login" element={<Login />} />
                 {/* Main routes for pages */}
-                <Route path='/' element={<MainLayout />}>
+                <Route path="/" element={<MainLayout />}>
                     <Route index element={<Home />} />
-                    <Route path='/products' element={<Products />} />
-                    <Route path='/product/:id' element={<ProductsPage />} />
-                    <Route path='/search' element={<SearchResult />} />
-                    <Route path='/deals' element={<FlashDeals />} />
+                    <Route path="/products" element={<Products />} />
+                    <Route path="/product/:id" element={<ProductsPage />} />
+                    <Route path="/search" element={<SearchResult />} />
+                    <Route path="/deals" element={<FlashDeals />} />
                 </Route>
                 <Route element={<ProtectedRoute />}>
-                    <Route path='/checkout' element={<Checkout />} />
-                    <Route path='/orders' element={<MyOrders />} />
-                    <Route path='/order/:id' element={<OrderTracking />} />
-                    <Route path='/addresses' element={<Address />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/orders" element={<MyOrders />} />
+                    <Route path="/order/:id" element={<OrderTracking />} />
+                    <Route path="/addresses" element={<Address />} />
                 </Route>
             </Routes>
         </>
